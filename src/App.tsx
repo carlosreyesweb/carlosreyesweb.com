9006a99b9294662aef7378dbda775479bca59809
Carlos Reyes <cgrt17062011@gmail.com>
2022-06-10 19:11:28 -0400

    1. Add branding stuff 2. Add CSS resets 3. Add public folder for static assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import logo from './assets/logo.svg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +9,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <h1>Hello Vite + React!</h1>
+        <h2>Welcome to my page.</h2>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
