79eac7d206ecbbee84e4f5d989ebefa1bec7af88
carlosreyesweb <cgrt17062011@gmail.com>
2022-07-09 16:06:22 -0400

    write 'about me' copy

--- src/components/me/me.component.tsx
+++ src/components/me/me.component.tsx
@@ -29,20 +29,27 @@ export default function Me(props: MeProps) {
       <article className={styles.article}>
         <h2 className={styles.title}>Who am I?</h2>
         <p className={styles.content}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-          accusantium sint? Saepe modi quasi placeat qui dolore omnis nam
-          laudantium, voluptas id sunt, quidem magni. Nostrum quos voluptate
-          facilis numquam. Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit.
+          Hi! My name is <strong>Carlos Reyes</strong>, as you might have
+          noticed before. I'm a <strong>Front-End Web Developer</strong> and a
+          passionate about web design and programming. I'm a{' '}
+          <strong>React specialist</strong>, but I also work with{' '}
+          <strong>Angular</strong>, and I'm constantly learning about new web
+          development ecosystems to get better on my area of expertise.
         </p>
         <p className={styles.content}>
-          Nisi corrupti vitae amet pariatur alias doloribus libero cum dolorum
-          deleniti! Recusandae blanditiis, eos nihil alias adipisci consectetur
-          quae laboriosam iusto deserunt! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Provident non ut in illo perferendis nostrum
-          molestias! Blanditiis unde alias ducimus animi error neque,
-          consectetur explicabo adipisci exercitationem temporibus sequi
-          quaerat.
+          I have worked in some projects for clients in countries such as
+          Australia, Brazil, United States and Venezuela, all of them in
+          collaboration with consulting companies such as{' '}
+          <a
+            href="https://neuralgeeks.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            neuralgeeks
+          </a>
+          . I can make{' '}
+          <strong>minimalist and eye-catching web interfaces</strong> that not
+          only look good, but also work flawlessly.
         </p>
         <ButtonLink
           variant="main-regular"
