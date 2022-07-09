bc6f4dd4fc29a312af5e6416c2499bb1ecb9f9da
carlosreyesweb <cgrt17062011@gmail.com>
2022-07-09 16:21:10 -0400

    write services section copy

--- src/components/services/services.component.tsx
+++ src/components/services/services.component.tsx
@@ -29,10 +29,14 @@ export default function Services(props: ServicesProps) {
           <img className={styles.serviceIcon} src={websiteIcon} alt="Website" />
           <h3 className={styles.serviceName}>Dynamic Websites</h3>
           <p className={styles.serviceDescription}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-            dolorem labore vitae nobis similique et, delectus molestiae velit
-            obcaecati architecto dignissimos dolore autem nesciunt sed quis!
-            Quisquam ex alias consectetur.
+            Websites could be more dynamic than they are nowadays.{' '}
+            <strong>
+              With me, you can create your own eye-catching, animated,
+              interactive website
+            </strong>
+            , no matter if it's a blog, an e-Commerce or your own portfolio; we
+            can make it impressive for your readers, potential clients or
+            recruiters.
           </p>
         </motion.article>
         <motion.article
@@ -44,10 +48,11 @@ export default function Services(props: ServicesProps) {
           <img className={styles.serviceIcon} src={pwaIcon} alt="PWA" />
           <h3 className={styles.serviceName}>Progressive Web Apps</h3>
           <p className={styles.serviceDescription}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-            dolorem labore vitae nobis similique et, delectus molestiae velit
-            obcaecati architecto dignissimos dolore autem nesciunt sed quis!
-            Quisquam ex alias consectetur.
+            A Progressive Web App (PWA) is an application that runs in the
+            browser, but it acts as a native application. It is cool for
+            projects like stores, content platforms, social media and other
+            heavily-interactive software ideas. If you think your idea fits
+            here, contact me!
           </p>
         </motion.article>
         <motion.article
@@ -56,13 +61,20 @@ export default function Services(props: ServicesProps) {
           whileInView="visible"
           className={styles.service}
         >
-          <img className={styles.serviceIcon} src={landingIcon} alt="Landing Page" />
+          <img
+            className={styles.serviceIcon}
+            src={landingIcon}
+            alt="Landing Page"
+          />
           <h3 className={styles.serviceName}>Landing Pages</h3>
           <p className={styles.serviceDescription}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-            dolorem labore vitae nobis similique et, delectus molestiae velit
-            obcaecati architecto dignissimos dolore autem nesciunt sed quis!
-            Quisquam ex alias consectetur.
+            No matter which type of project you got in mind;{' '}
+            <strong>
+              you need a landing page, a site where your potential clients could
+              become real clients.
+            </strong>{' '}
+            It is your cover letter, your face in front of new people in your
+            target audience. We can make it real in almost no time!
           </p>
         </motion.article>
       </section>
