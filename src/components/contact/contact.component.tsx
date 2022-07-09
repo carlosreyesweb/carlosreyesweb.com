fc490664693c89abcb9f341855d5aed5b0a770ab
carlosreyesweb <cgrt17062011@gmail.com>
2022-07-09 15:15:29 -0400

    add contact section

--- src/components/contact/contact.component.tsx
+++ src/components/contact/contact.component.tsx
@@ -1,18 +1,45 @@
+import gmailIcon from '../../assets/gmail-icon.svg'
+import linkedinIcon from '../../assets/linkedin-icon.svg'
+import whatsappIcon from '../../assets/whatsapp-icon.svg'
+import ButtonLink from '../button-link/button-link.component'
 import styles from './contact.component.module.css'
 
 interface ContactProps {}
 export default function Contact(props: ContactProps) {
   return (
     <section id="contact" className={styles.container}>
-      <h2 className={styles.title}>
-        One step closer to achieve impressive web presence.
-      </h2>
-      <p className={styles.content}>Fill the form below to contact me.</p>
-      {/* <section className={styles.formContainer}>
-        <form className={styles.form}>
-          <h3 className={styles.formTitle}>Contact form</h3>
-        </form>
-      </section> */}
+      <h2 className={styles.title}>Are you ready?</h2>
+      <p className={styles.content}>
+        Click one of the buttons below to contact me!
+      </p>
+      <nav className={styles.linksContainer}>
+        <ButtonLink
+          variant="main-regular"
+          href="mailto:contact@carlosreyesweb.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={gmailIcon} alt="Gmail" className={styles.linkIcon} /> E-mail
+        </ButtonLink>
+        <ButtonLink
+          variant="main-regular"
+          href="https://wa.me/+584129380768"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={whatsappIcon} alt="WhatsApp" className={styles.linkIcon} />{' '}
+          WhatsApp
+        </ButtonLink>
+        <ButtonLink
+          variant="main-regular"
+          href="https://linkedin.com/in/carlosreyesweb"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedinIcon} alt="LinkedIn" className={styles.linkIcon} />{' '}
+          LinkedIn
+        </ButtonLink>
+      </nav>
     </section>
   )
 }
